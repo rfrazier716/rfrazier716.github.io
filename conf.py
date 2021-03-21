0faf57abd81a4d9d20041d881190a4c6be691118
RyanFrazier <rfrazier716@gmail.com>
2021-03-21 11:06:54 -0400

    changed name to fotonix and Gizmos

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Ryan Frazier"  # (translatable)
-BLOG_TITLE = "Fotonix & Gizmo"  # (translatable)
+BLOG_TITLE = "Fotonix & Gizmos"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://rfrazier716.github.io/"
